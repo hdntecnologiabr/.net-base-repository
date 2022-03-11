92580f32849ce1d002a9baef558f6cc92b6c7a94
Mike Cruvinel <mike.queiroz@hdn.digital>
2022-03-11 16:35:26 -0300

    add fixtures

--- tests/Hdn.Core.Architecture.Template.Aplication.Test/TodoItem/Handlers/CreateTodoItemCommandHandler.Test.cs
+++ tests/Hdn.Core.Architecture.Template.Aplication.Test/TodoItem/Handlers/CreateTodoItemCommandHandler.Test.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoFixture;
 using FluentAssertions;
 using Hdn.Core.Architecture.Application.TodoItem.Commands.CreateTodoItem;
 using Hdn.Core.Architecture.Application.TodoItem.Handlers;
@@ -14,32 +15,38 @@ namespace Hdn.Core.Architecture.Application.Tests.TodoItem.Handlers;
 [Collection("Testing TodoItemCommand")]
 public class CreateTodoItemCommandHandlerTest
 {
-
     public CreateTodoItemCommandHandlerTest()
     {
 
     }
 
-    [Fact(DisplayName = "Objeto do tipo TodoItemEntity valido irá Executar metodo TodoItemCreateHandle e deverá retornar um Id valido")]
+    [Fact(DisplayName = "Dado que o objeto CreateTodoItemCommand é valido, quando o metodo create é chamado, então deverá retornar TodoItemEntity.Id valido")]
     public async Task Should_Return_Success_ToDoItemHandler_Create()
     {
         //Arrange
-        CreateTodoItemCommand request = new CreateTodoItemCommand { ListId = Guid.Parse("2f02ffb6-1769-4ef1-80d5-07fe4b64db15"), Title = "teste" };
-        Mock<ITodoItemRepository> todoItemRepository = new Mock<ITodoItemRepository>();
+        var fixture = new Fixture();
 
-        var entityToCreate = new TodoItemEntity
-        {
-            ListId = Guid.Parse("2f02ffb6-1769-4ef1-80d5-07fe4b64db15"),
-            Title = "teste",
-            Id = Guid.Parse("fe87aa6d-4ec8-4224-af21-59d8acffdf60")
-        };
+        fixture.Customize<CreateTodoItemCommand>(c =>
+            c.With(p => p.ListId, Guid.Parse("2f02ffb6-1769-4ef1-80d5-07fe4b64db15"))
+            .With(p => p.Title, "teste"));
+
+        var request = fixture.Create<CreateTodoItemCommand>();
+
+        fixture.Customize<TodoItemEntity>(c => 
+            c.With(p => p.ListId, Guid.Parse("2f02ffb6-1769-4ef1-80d5-07fe4b64db15"))
+            .With(p => p.Title, "teste")
+            .With(p => p.Id, Guid.Parse("fe87aa6d-4ec8-4224-af21-59d8acffdf60")));
+
+        var entityToCreate = fixture.Create<TodoItemEntity>();
+
+        var todoItemRepository = new Mock<ITodoItemRepository>();
 
         var cancelationToken = new CancellationToken();
 
         todoItemRepository.Setup(x => x.InsertAsync(It.IsAny<TodoItemEntity>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(entityToCreate);
 
-        CreateTodoItemCommandHandler commandHandler = new CreateTodoItemCommandHandler(todoItemRepository.Object);
+        var commandHandler = new CreateTodoItemCommandHandler(todoItemRepository.Object);
 
         //Act
         var entityResponse = await commandHandler.Handle(request, cancelationToken);
@@ -51,29 +58,33 @@ public class CreateTodoItemCommandHandlerTest
         todoItemRepository.Verify(x => x.InsertAsync(It.IsAny<TodoItemEntity>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 
-    [Fact(DisplayName ="Objeto do tipo TodoItemEntity invalido irá executar metodo TodoItemCreateHandle e deverá retornar exception")]
+    [Fact(DisplayName ="Dado que o objeto CreateTodoItemCommand é invalido, quando o metodo create é chamado, então deverá retorar TodoItemEntity.Id invalido")]
     public async Task Should_Fail_ToDoItemHandler_Create()
     {
         //Arrange
-        CreateTodoItemCommand request = new CreateTodoItemCommand { 
-            ListId = Guid.Empty,
-            Title = "Teste"
-        };
+        var fixture = new Fixture();
 
-        Mock<ITodoItemRepository> todoItemRepository = new Mock<ITodoItemRepository>();
+        fixture.Customize<CreateTodoItemCommand>(c =>
+            c.With(p => p.ListId, Guid.Empty)
+            .With(p => p.Title, "testeFail"));
 
-        var entityToCreate = new TodoItemEntity
-        {
-            ListId = Guid.Parse("fe87aa6d-4ec8-4224-af21-59d8acffdf60"),
-            Title = "testeFail",
-        };
+        var request = fixture.Create<CreateTodoItemCommand>();
+
+        fixture.Customize<TodoItemEntity>(c =>
+            c.With(p => p.ListId, Guid.Parse("fe87aa6d-4ec8-4224-af21-59d8acffdf60"))
+            .With(p => p.Title, "teste")
+            .With(p => p.Id, Guid.Empty).With(p => p.List, new TodoListEntity()));
+
+        var entityToCreate = fixture.Create<TodoItemEntity>();
+
+        var todoItemRepository = new Mock<ITodoItemRepository>();
 
         var cancelationToken = new CancellationToken();
 
         todoItemRepository.Setup(x => x.InsertAsync(It.IsAny<TodoItemEntity>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(entityToCreate);
 
-        CreateTodoItemCommandHandler commandHandler = new CreateTodoItemCommandHandler(todoItemRepository.Object);
+        var commandHandler = new CreateTodoItemCommandHandler(todoItemRepository.Object);
 
         //Act
         var entityResponse = await commandHandler.Handle(request, cancelationToken);
@@ -82,6 +93,7 @@ public class CreateTodoItemCommandHandlerTest
         entityResponse.Should().BeEmpty();
         entityToCreate.ListId.Should().NotBe(request.ListId);
         entityToCreate.Title.Should().NotBe(request.Title);
+        todoItemRepository.Verify(x => x.InsertAsync(It.IsAny<TodoItemEntity>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
 
